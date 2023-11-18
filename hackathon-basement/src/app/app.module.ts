318c212a8a92804cdc6069cedb5a9bcb5d13f166
Michał Rusak <104869035+michalrusak@users.noreply.github.com>
2023-11-18 11:21:28 +0100

    add spinner

--- hackathon-basement/src/app/app.module.ts
+++ hackathon-basement/src/app/app.module.ts
@@ -3,7 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import {
+  HttpClientModule,
+  HttpClient,
+  HTTP_INTERCEPTORS,
+} from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { FormsModule } from '@angular/forms';
 import { StudentModule } from './student/student.module';
@@ -11,6 +15,8 @@ import { PdfCreatorComponent } from './student/components/pdf-creator/pdf-creato
 import { CoreModule } from './core/core.module';
 import { AuthModule } from './auth/auth.module';
 import { HomeModule } from './home/home.module';
+import { SharedModule } from './shared/shared.module';
+import { SpinnerInterceptor } from './shared/spinner/interceptor/spinner';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
@@ -33,9 +39,10 @@ export function HttpLoaderFactory(http: HttpClient) {
     }),
     FormsModule,
     StudentModule,
-    HomeModule
+    HomeModule,
+    SharedModule,
   ],
-  providers: [],
+
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- hackathon-basement/src/app/shared/shared.module.ts
+++ hackathon-basement/src/app/shared/shared.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlertComponent } from './alert/alert.component';
 import { MessagesModule } from 'primeng/messages';
+import { SpinnerComponent } from './spinner/component/spinner/spinner.component';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { SpinnerInterceptor } from './spinner/interceptor/spinner';
 
 @NgModule({
-  declarations: [AlertComponent],
-  imports: [CommonModule, MessagesModule],
-  exports: [AlertComponent],
+  declarations: [AlertComponent, SpinnerComponent],
+  imports: [CommonModule, MessagesModule, ProgressSpinnerModule],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: SpinnerInterceptor, multi: true },
+  ],
+  exports: [AlertComponent, SpinnerComponent],
 })
 export class SharedModule {}
