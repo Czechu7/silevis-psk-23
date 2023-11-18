a13aa3299cda50b528d20ebda87c20124c6ffcea
Czechu7 <choocas0@gmail.com>
2023-11-18 03:43:43 +0100

    nowy header + login page
    nowy header + login page #1

--- hackathon-basement/src/app/app.component.ts
+++ hackathon-basement/src/app/app.component.ts
@@ -1,22 +1,37 @@
-import { Component } from '@angular/core';
+// app.component.ts
+import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { AuthService } from './auth/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title(title: any) {
-    throw new Error('Method not implemented.');
+export class AppComponent implements OnInit{
+  islogged$ = false;
+  ngOnInit() {
+    // Tutaj możesz zasubskrybować się do BehaviorSubject
+    this.authService.loggedUser$.subscribe((loggedIn: boolean) => {
+      this.islogged$ = loggedIn;
+
+    });
   }
-  constructor(translate: TranslateService) {
+  isLoginPage(): boolean {
+    return this.router.url.includes('/login');
+  }
+  
+
+  constructor(private authService: AuthService, translate: TranslateService, private router: Router) {
     translate.addLangs(['en', 'pl']);
     translate.setDefaultLang('pl');
 
     // Defaultowo tłumaczy na polski
-
     const browserLang = translate.getBrowserLang();
     translate.use(browserLang?.match(/pl|en/) ? browserLang : 'pl');
+
+    
+ 
   }
 }
