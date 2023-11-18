23cb43726531af19ebb6cf592ae9c49f6cef75e8
Konrad Gliński <schodek1234@gmail.com>
2023-11-18 01:28:32 +0100

    header lang

--- hackathon-basement/src/app/core/partials/header/header.component.ts
+++ hackathon-basement/src/app/core/partials/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,19 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  constructor(public translate: TranslateService) {}
+items: MenuItem[]|undefined;
+selectedImage: string;
+  constructor(public translate: TranslateService) {
+    this.selectedImage = this.getImagePath(translate.currentLang);
 
-  changeLanguage(lang: string): void {
-    this.translate.use(lang);
+  }
+
+  changeLanguage(selectedLang: string): void {
+    this.translate.use(selectedLang);
+    this.selectedImage = this.getImagePath(selectedLang);
+  }
+
+  getImagePath(lang: string): string {
+    return `assets/${lang.toLowerCase()}.png`;
   }
 }
